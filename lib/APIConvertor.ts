c103128e028c86ca0f08d54559587a744ea9d4d6
Electroplayer <vitaly.hawanski@yandex.ru>
2024-08-26 20:59:03 +0300

    Добавлены интерфейсы

--- lib/APIConvertor.ts
+++ lib/APIConvertor.ts
@@ -1,94 +1,135 @@
-import fetch from "node-fetch";
-import https from "https";
+import fetch from 'node-fetch';
+import https from 'https';
 
 interface IAPIResp<T> {
-    "isok": boolean,
-    "data": T,
-    "error_message": string | null
+    isok: boolean;
+    data: T;
+    error_message: string | null;
+}
+
+export interface IRespOFOPara {
+    nedtype: {
+        nedtype_id: number;
+        nedtype_name: string;
+    };
+    dayofweek: {
+        dayofweek_id: number;
+        dayofweek_name: string;
+    };
+    pair: number;
+    kindofnagr: {
+        kindofnagr_id: number;
+        kindofnagr_name: string;
+    };
+    disc: {
+        disc_id: number;
+        disc_name: string;
+    };
+    ned_from: number;
+    ned_to: number;
+    persent_of_gr: number;
+    ispotok: boolean;
+    classroom: string;
+    isdistant: boolean;
+    teacher: string;
+    comment: string;
 }
 
 interface IRespExam {
-    "date_sd": string,
-    "time_sd": string,
-    "disc": {
-        "disc_id": number,
-        "disc_name": string
-    },
-    "classroom": string,
-    "teacher": string
+    date_sd: string;
+    time_sd: string;
+    disc: {
+        disc_id: number;
+        disc_name: string;
+    };
+    classroom: string;
+    teacher: string;
 }
 
 interface IRespInst {
-    "id": number,
-    "name": string,
-    "fname": string
+    id: number;
+    name: string;
+    fname: string;
 }
 
 interface IRespGroup {
-    "id": number,
-    "name": string,
-    "inst_id": number,
-    "formaob_id": number,
-    "kurs": number
+    id: number;
+    name: string;
+    inst_id: number;
+    formaob_id: number;
+    kurs: number;
 }
 
 const opts = {
     headers: {
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
     },
-    agent: new https.Agent({ rejectUnauthorized: false })
+    agent: new https.Agent({ rejectUnauthorized: false }),
 };
 
-export async function ofo(gr: string, ugod: string | number = new Date().getFullYear() - (new Date().getMonth() >= 6 ? 0 : 1), sem: string | number = new Date().getMonth() > 5 ? 1 : 2) {
-    let resp = await fetch(`${process.env.KUBSTU_API}/timetable/ofo?gr=${gr}&ugod=${ugod}&semestr=${sem}`, opts)
-    .catch(console.log);
+export async function ofo(
+    gr: string,
+    ugod: string | number = new Date().getFullYear() - (new Date().getMonth() >= 6 ? 0 : 1),
+    sem: string | number = new Date().getMonth() > 5 ? 1 : 2,
+) {
+    let resp = await fetch(`${process.env.KUBSTU_API}/timetable/ofo?gr=${gr}&ugod=${ugod}&semestr=${sem}`, opts).catch(console.log);
 
-    if(resp) {
-        let json: IAPIResp<IRespOFOPara[]> = await resp.json() as IAPIResp<IRespOFOPara[]>;
+    if (resp) {
+        let json: IAPIResp<IRespOFOPara[]> = (await resp.json()) as IAPIResp<IRespOFOPara[]>;
 
-        json.data.map(elm => {
-            if(!elm.teacher.trim()) elm.teacher = 'Не назначен';
-            if(!elm.classroom.trim()) elm.teacher = 'Не назначена';
+        json.data.map((elm) => {
+            if (!elm.teacher.trim()) elm.teacher = 'Не назначен';
+            if (!elm.classroom.trim()) elm.teacher = 'Не назначена';
 
             return elm;
         });
 
         return json;
-    }
-    else return undefined;
+    } else return undefined;
 }
 
-export async function exam(gr: string, ugod: string | number = new Date().getFullYear() - (new Date().getMonth() >= 6 ? 0 : 1), sem: string | number = new Date().getMonth() > 5 ? 1 : 2) {
-    let resp = await fetch(`${process.env.KUBSTU_API}/timetable/exam?gr=${gr}&ugod=${ugod}&semestr=${sem}`, opts)
-    .catch(console.log);
+export async function exam(
+    gr: string,
+    ugod: string | number = new Date().getFullYear() - (new Date().getMonth() >= 6 ? 0 : 1),
+    sem: string | number = new Date().getMonth() > 5 ? 1 : 2,
+) {
+    let resp = await fetch(`${process.env.KUBSTU_API}/timetable/exam?gr=${gr}&ugod=${ugod}&semestr=${sem}`, opts).catch(console.log);
 
-    if(resp) return await resp.json() as IAPIResp<IRespExam[]>;
+    if (resp) return (await resp.json()) as IAPIResp<IRespExam[]>;
     else return undefined;
 }
 
 export async function instList() {
-    let resp = await fetch(`${process.env.KUBSTU_API}/timetable/inst-list`, opts)
-    .catch(console.log);
+    let resp = await fetch(`${process.env.KUBSTU_API}/timetable/inst-list`, opts).catch(console.log);
 
-    if(resp) return await resp.json() as IAPIResp<IRespInst[]>;
+    if (resp) return (await resp.json()) as IAPIResp<IRespInst[]>;
     else return undefined;
 }
 
-export async function ofoGroupsList(ugod: number | string = new Date().getFullYear() - (new Date().getMonth() >= 6 ? 0 : 1), inst_id?: string | number, kurs?: string | number) {
-    let resp = await fetch(`${process.env.KUBSTU_API}/timetable/gr-list?ugod=${ugod}&formaob_id=1${inst_id ? `&inst_id=${inst_id}` : ''}${inst_id ? `&kurs=${kurs}` : ''}`, opts)
-    .catch(console.log);
+export async function ofoGroupsList(
+    ugod: number | string = new Date().getFullYear() - (new Date().getMonth() >= 6 ? 0 : 1),
+    inst_id?: string | number,
+    kurs?: string | number,
+) {
+    let resp = await fetch(
+        `${process.env.KUBSTU_API}/timetable/gr-list?ugod=${ugod}&formaob_id=1${inst_id ? `&inst_id=${inst_id}` : ''}${inst_id ? `&kurs=${kurs}` : ''}`,
+        opts,
+    ).catch(console.log);
 
-    if(resp) {
-        let json = await resp.json() as IAPIResp<IRespGroup[]>;
+    if (resp) {
+        let json = (await resp.json()) as IAPIResp<IRespGroup[]>;
 
-        if(!json.isok) return json;
+        if (!json.isok) return json;
         // Из-за какого-то бага, formaob_id=1 не работает, поэтому производим фильтрацию прямо тут
-        json.data = json.data.filter(g => g.formaob_id == 1);
+        json.data = json.data.filter((g) => g.formaob_id == 1);
 
         return json;
     } else return undefined;
 }
 
 export default {
-    ofo, exam, instList, ofoGroupsList
+    ofo,
+    exam,
+    instList,
+    ofoGroupsList,
 };
--- structures/Group.ts
+++ structures/Group.ts
@@ -1,28 +1,40 @@
-import Schedules from "../models/ScheduleModel.js";
-import Groups from "../models/GroupsModel.js";
-import APIConvertor from "../lib/APIConvertor.js";
-import { genToken } from "../lib/Utils.js";
+import Schedules from '../models/ScheduleModel.js';
+import Groups from '../models/GroupsModel.js';
+import APIConvertor, { IRespOFOPara } from '../lib/APIConvertor.js';
+import { genToken } from '../lib/Utils.js';
 // import ExamsModel from "../models/ExamsModel.js";
 
 export default class BaseGroup {
     kurs: number;
     cachedFullRawSchedule?: {
-        data: IRespOFOPara[],
-        updateDate: Date
-    };
-    
-    static lessonsTime: string[] = ["what?", "8:00 - 9:30", "9:40 - 11:10", "11:20 - 12:50", "13:20 - 14:50", "15:00 - 16:30", "16:40 - 18:10", "18:20 - 19:50"];
-    static lessonsTypes: {[key:string]: string} = {
-        "Лекции": "Лекция",
-        "Практические занятия": "Практика",
-        "Лабораторные занятия": "Лабораторная"
+        data: IRespOFOPara[];
+        updateDate: Date;
     };
 
-    constructor(public name: string, public instId: number) {
-        let year = +(name[0]+name[1]);
-        let now  = new Date();
+    static lessonsTime: string[] = [
+        'what?',
+        '8:00 - 9:30',
+        '9:40 - 11:10',
+        '11:20 - 12:50',
+        '13:20 - 14:50',
+        '15:00 - 16:30',
+        '16:40 - 18:10',
+        '18:20 - 19:50',
+    ];
+    static lessonsTypes: { [key: string]: string } = {
+        Лекции: 'Лекция',
+        'Практические занятия': 'Практика',
+        'Лабораторные занятия': 'Лабораторная',
+    };
 
-        this.kurs    = now.getUTCFullYear() - 2000 - (now.getUTCMonth() >= 6 ? 0 : 1) - year + 1; // FIXME: Будет работать до 2100 года
+    constructor(
+        public name: string,
+        public instId: number,
+    ) {
+        let year = +(name[0] + name[1]);
+        let now = new Date();
+
+        this.kurs = now.getUTCFullYear() - 2000 - (now.getUTCMonth() >= 6 ? 0 : 1) - year + 1; // FIXME: Будет работать до 2100 года
     }
 
     /**
@@ -31,24 +43,26 @@ export default class BaseGroup {
      * Если в БД расписания нет, возвращает undefined
      */
     async getFullRawSchedule() {
-        let date  = new Date();
+        let date = new Date();
 
-        if(this.cachedFullRawSchedule && date.valueOf() - this.cachedFullRawSchedule.updateDate.valueOf() < 1000 * 60 * 60 * 4) return this.cachedFullRawSchedule.data;
-        
-        let ugod  = date.getFullYear() - (date.getMonth() >= 6 ? 0 : 1);
-        let sem   = date.getMonth() > 5 ? 1 : 2;
+        if (this.cachedFullRawSchedule && date.valueOf() - this.cachedFullRawSchedule.updateDate.valueOf() < 1000 * 60 * 60 * 4)
+            return this.cachedFullRawSchedule.data;
+
+        let ugod = date.getFullYear() - (date.getMonth() >= 6 ? 0 : 1);
+        let sem = date.getMonth() > 5 ? 1 : 2;
 
         let resp = await APIConvertor.ofo(this.name, ugod, sem);
 
-        if(!resp || !resp.isok) {
+        if (!resp || !resp.isok) {
             let dbResponse = await Schedules.findOne({ group: this.name }).exec();
 
             // Если расписание есть в БД, кешируем его только на час
             // Если убрать кеш ответа из БД, бот постоянно биться в неработающий сайт
-            if(dbResponse) this.cachedFullRawSchedule = {
-                data: dbResponse.data as IRespOFOPara[],
-                updateDate: new Date(date.valueOf() - 1000*60*60*3)
-            };
+            if (dbResponse)
+                this.cachedFullRawSchedule = {
+                    data: dbResponse.data as IRespOFOPara[],
+                    updateDate: new Date(date.valueOf() - 1000 * 60 * 60 * 3),
+                };
 
             return dbResponse?.data as IRespOFOPara[] | undefined;
         } else {
@@ -60,27 +74,30 @@ export default class BaseGroup {
         }
     }
 
-    async getDayRawSchedule(day = new Date().getDay(), week = (new Date().getWeek()%2==0)) {
+    async getDayRawSchedule(day = new Date().getDay(), week = new Date().getWeek() % 2 == 0) {
         let fullSchedule = await this.getFullRawSchedule();
 
-        if(!fullSchedule) return undefined;
-        else return fullSchedule
-        .filter(p => p.nedtype.nedtype_id == (week ? 2 : 1) && p.dayofweek.dayofweek_id == day)
-        .sort((a,b) => a.pair - b.pair);
+        if (!fullSchedule) return undefined;
+        else
+            return fullSchedule
+                .filter((p) => p.nedtype.nedtype_id == (week ? 2 : 1) && p.dayofweek.dayofweek_id == day)
+                .sort((a, b) => a.pair - b.pair);
     }
-    
-    async getToken():Promise<string> {
-        let groupInfo = await Groups.findOne({group: this.name, inst_id: this.instId}).exec();
 
-        if(groupInfo) return groupInfo.token;
+    async getToken(): Promise<string> {
+        let groupInfo = await Groups.findOne({ group: this.name, inst_id: this.instId }).exec();
+
+        if (groupInfo) return groupInfo.token;
         else {
             let token = genToken(this.name, this.instId);
 
             new Groups({
                 group: this.name,
                 inst_id: this.instId,
-                token
-            }).save().catch(console.log);
+                token,
+            })
+                .save()
+                .catch(console.log);
 
             return token;
         }
